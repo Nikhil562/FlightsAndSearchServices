465cd153386f7c54160947d22c58edc15e1e3400
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-05-12 16:18:51 +0530

    Added common crud and middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const CityController = require('../../controllers/city-controller');
 const FlightController = require('../../controllers/flight-controller');
-
+const AirportController = require('../../controllers/airport-controller');
+const { FlightMiddlewares } = require('../../middlewares/index');
 const router = express.Router();
 
 router.post('/city', CityController.create);
 router.delete('/city/:id', CityController.destroy);
 router.get('/city/:id', CityController.get);
-router.patch('/city/:id', CityController.update);
 router.get('/city', CityController.getAll);
-router.post('/flights', FlightController.create);
+router.patch('/city/:id', CityController.update);
+
+router.post(
+    '/flights', 
+    FlightMiddlewares.validateCreateFlight, 
+    FlightController.create
+);
+
 router.get('/flights', FlightController.getAll);
 
+router.post('/airports', AirportController.create);
 
 module.exports = router;
